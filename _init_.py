041608dbc1d7c1d0ab017cec649ca595575cc260
admin <admin@admindeMacBook-Pro.local>
2019-07-09 12:19:02 +0800

    删除多余的import

--- _init_.py
+++ _init_.py
@@ -5,7 +5,7 @@ from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'MyFlaskApplication' # 当要使用Session或Cookies的时候需要这个配置，后面随便设置
+app.config['SECRET_KEY'] = 'MyFlaskApplication!' # 当要使用Session或Cookies的时候需要这个配置，后面随便设置
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:123@127.0.0.1:3306/[MySQL db name]" # 连接SQL的URI，后面自行填写相关SQL信息
 app.config['JSON_AS_ASCII'] = False # 不转换为ASCII码输出，而是保留Unicode编码原样输出
 
